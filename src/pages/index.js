0941486f216df9bbf65d252b41f32fb70059ece5
EmanuelStoef <150154248+EmanuelStoef@users.noreply.github.com>
2025-12-11 13:58:12 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "./components/cards";
 export async function getServerSideProps() {
   // Fetch data from  API notícias
-  const res = await fetch('https://projectnext5b.vercel.app/api/noticias/apinoticias')
+  const res = await fetch('https://apiemanuelinfb.vercel.app/noticias')
   const repo = await res.json()
   // Pass data to the page via props
   return { props: { noticias: repo } }
