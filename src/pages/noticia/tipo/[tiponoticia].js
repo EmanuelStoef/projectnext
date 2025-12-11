258ac1149503ba9cc1b07f2f545f07833900c3ce
EmanuelStoef <150154248+EmanuelStoef@users.noreply.github.com>
2025-12-11 13:59:51 -0400

    Update [tiponoticia].js

--- src/pages/noticia/tipo/[tiponoticia].js
+++ src/pages/noticia/tipo/[tiponoticia].js
@@ -2,7 +2,7 @@ import { Container, Row } from "react-bootstrap";
 import Cards from "../../components/cards";
 export async function getServerSideProps(req) {
   // Fetch data from  API notícias
-  const res = await fetch('https://projectnext5b.vercel.app/api/noticias/tipo/' + req.query.tiponoticia)
+  const res = await fetch('https://apiemanuelinfb.vercel.app/noticias/tiponoticia/' + req.query.tiponoticia)
   const repo = await res.json()
   // Pass data to the page via props ola
   return { props: { noticias: repo } }
